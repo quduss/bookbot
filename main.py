221caa6a8705eb7b2000f33c122032aa27decdf5
quduss <shuaib.qudus@yahoo.com>
2025-02-05 09:44:51 +0100

    Print an overall report

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     num_words = get_length(file_contents)
-    num_characters_dict = count_characters(file_contents)
-    print(num_characters_dict)
+    characters_dict = count_characters(file_contents)
+    print_report(characters_dict, num_words)
 
 def get_length(string):
     words = string.split()
@@ -19,5 +19,16 @@ def count_characters(string):
         else:
             my_dict[c] = 1
     return my_dict
+
+def print_report(my_dict, total_words):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{total_words} words found in the document")
+    print()
+    sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
+    for key in sorted_dict:
+        if key.isalpha():
+            print(f"The '{key}' character was found {sorted_dict[key]} times")
+    print("--- End report ---")
+        
 
 main()
