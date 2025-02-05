ba4138d4c396c696b0476f7eae0cfefbcaa66268
quduss <shuaib.qudus@yahoo.com>
2025-02-05 09:08:58 +0100

    Count number of each characters in the book

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(get_length(file_contents))
+    num_words = get_length(file_contents)
+    num_characters_dict = count_characters(file_contents)
+    print(num_characters_dict)
 
 def get_length(string):
     words = string.split()
     number_of_words = len(words)
     return number_of_words
+
+def count_characters(string):
+    lowered_string = string.lower()
+    my_dict = {}
+    for c in lowered_string:
+        if c in my_dict:
+            my_dict[c] += 1
+        else:
+            my_dict[c] = 1
+    return my_dict
 
 main()
